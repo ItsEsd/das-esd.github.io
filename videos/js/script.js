22180c49263f8e00acd4b41400098a956358da31
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-12-10 14:34:41 +0530

    Update script.js

--- videos/js/script.js
+++ videos/js/script.js
@@ -7,14 +7,12 @@ var vidset =[
 "artde",
 "tinbo",
 "labre",
-"dhdhr"];
+"dhdhr",
+"hmiss"];
 function thisvid(label){
     var elem = document.getElementsByClassName('setvid');
     elem = [].slice.call(elem);
     var pos = elem.indexOf(label);
-    console.log(pos); 
     var ln = "v/?vsrc="+vidset[pos]+"&q=true";
-
-    console.log(ln);
     window.open(ln,'_self')
     }
